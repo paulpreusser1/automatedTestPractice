4a118150a2ad56b66c32ae6f6e4f80ddb916f04e
Paul Preusser <paulpreusser@me.com>
2023-12-17 19:12:41 +0100

    Initial commit

---
+++ src/example.ts
@@ -0,0 +1,11 @@
+import { chromium } from 'playwright';
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
